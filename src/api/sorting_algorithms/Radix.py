cc5b623ecabee143d2109a067aedf72a4100f0f8
Carlospq17 <carlospq_17@hotmail.com>
2022-09-27 00:47:34 -0500

    Remove comments in function

--- src/api/sorting_algorithms/Radix.py
+++ src/api/sorting_algorithms/Radix.py
@@ -7,16 +7,13 @@ class Radix(SortInterface):
         output = [0] * size
         count = [0] * 10
 
-        # Calculate count of elements
         for i in range(0, size):
             index = array[i] // place
             count[index % 10] += 1
 
-        # Calculate cumulative count
         for i in range(1, 10):
             count[i] += count[i - 1]
 
-        # Place the elements in sorted order
         i = size - 1
         while i >= 0:
             index = array[i] // place
