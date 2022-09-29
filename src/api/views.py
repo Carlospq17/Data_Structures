8b3836ab7ef54af7f34fcd59a334c6b516e2cc38
Carlospq17 <carlospq_17@hotmail.com>
2022-09-28 22:26:49 -0500

    Implement get method

--- src/api/views.py
+++ src/api/views.py
@@ -1,30 +1,44 @@
 from django.views import View
-from django.http import JsonResponse
+from django.http import HttpResponse
 
 import json
+import importlib
+
+from api.sorting_algorithms.Proxy import Proxy
+from api.sorting_algorithms.SortInterface import SortInterface
 
 # Create your views here.
 
-class product_call(View):
+class sorting_methods(View):
     
     def get(self, request):
-        pass
+        json_data = json.loads(request.body)
+        size = json_data['size']
+        sorting_methods = json_data['sorting_methods']
+        data = {}
+
+        for element in sorting_methods:
+            try:
+                method = getattr(importlib.import_module('api.sorting_algorithms.'+element), element)
+                proxy = Proxy(method())
+                original_array = proxy.generateExampleArray(size)
+                sorted_array = original_array.copy()
+                proxy.sort(sorted_array)
+            except:
+                raise NameError("%s doesn't exist." % element)
+            finally:
+                if isinstance(proxy, (SortInterface)):
+                    data[element] = {
+                        'time' : proxy.getTime(),
+                        'original_array' : original_array,
+                        'sorted_array' : sorted_array
+                    }
+                else:
+                    raise TypeError("%s is not a class." % element)
+        resp = {}
+        resp['data'] = data
+        return HttpResponse(json.dumps(resp), content_type="application/json")
+
 
     def post(self, request):
         pass
-
-    def put(self, request):
-        pass
-
-    def delete(self, request):
-        pass
-
-# try:
-#     object = getattr(importlib.import_module('api.models'), product)
-#     declared_object = object()
-#     b = declared_object.op2()
-# except AttributeError:
-#     raise NameError("%s doesn't exist." % product)
-# if isinstance(declared_object, (Creator)):
-#     return JsonResponse(b, safe=False)
-# raise TypeError("%s is not a class." % product)
